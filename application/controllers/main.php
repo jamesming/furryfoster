75db626a4fb68bea00e1cfc9a43c6fedeb908811
James Ming <jamesming@gmail.com>
2013-02-10 11:01:27 -0800

    test

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends Base_Controller {
+class Main extends Base_Controller {// test
 	
 	function __construct() {
 		
